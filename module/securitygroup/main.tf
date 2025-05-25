00783ecf8cdca935355397d01007a26bf034a49f
mzmarci <mzmazy100@gmail.com>
2025-05-25 02:56:44 +0100

    updating

--- module/securitygroup/main.tf
+++ module/securitygroup/main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group_rule" "web_ingress3" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-  source_security_group_id = aws_security_group.stock_security_group.id 
+  source_security_group_id = aws_security_group.lb_security_group.id 
 }
 
 resource "aws_security_group_rule" "web_ingress" {
